39b29680852c3a96a002f3d3c5739b558d9c2c3c
Sofia_L <2964162259@qq.com>
2020-12-16 18:36:22 +0800

    001

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -4,4 +4,18 @@ using UnityEngine;
 
 public class PlayerData : MonoBehaviour
 {
+    private static PlayerData player;
+
+    public static PlayerData Player
+    {
+        get
+        {
+            return player;
+        }
+    }
+     public int GetDepots()
+    {
+        int getdepot = 0;
+        return getdepot;
+    }
 }
